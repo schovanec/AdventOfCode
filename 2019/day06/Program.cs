a3747a5caab84378ded4f3397fc0f816fcb773cc
Steve Chovanec <steve@stevechovanec.com>
2020-03-06 20:02:14 -0500

    Date 6 part 2

--- 2019/day06/Program.cs
+++ 2019/day06/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 
@@ -13,40 +14,87 @@ namespace day06
                 ? File.ReadLines(args.First()).Where(x => !string.IsNullOrEmpty(x))
                 : TestInput;
 
-            var orbits = input.Select(x => x.ToUpper().Split(')'))
-                              .Where(x => x.Length == 2)
-                              .Select(x => (inner: x[0], outer: x[1]))
-                              .Distinct()
-                              .ToLookup(x => x.inner, x => x.outer);
+            var graph = BuildEdgeMap(ParseOrbits(input));
 
-            foreach (var group in orbits)
+            var path = FindShortestPath("YOU", "SAN", graph);
+
+            if (path != null)
             {
-                foreach (var item in group)
+                foreach (var item in path)
+                    Console.WriteLine($"{item.from} to {item.to}");
+
+                Console.WriteLine();
+                Console.WriteLine($"Length = {path.Count}");
+            }
+            else
+            {
+                Console.WriteLine("Not found!");
+            }
+
+            // var totalOrbits = CountTotalOrbits(orbits);
+            // Console.WriteLine(totalOrbits);
+        }
+
+        private static ImmutableList<(string from, string to)> FindShortestPath(string from, string to, ILookup<string, string> adjacent)
+        {
+            var parent = new Dictionary<string, string> { [from] = null };
+            var queue = new Queue<string>();
+            queue.Enqueue(from);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                if (current == to)
                 {
-                    Console.WriteLine($"{group.Key} ) {item}");
+                    var result = ImmutableList.CreateBuilder<(string, string)>();
+                    var next = parent[current];
+                    if (next != from)
+                    {
+                        var prev = next;
+                        next = parent[prev];
+                        while (next != from)
+                        {
+                            result.Insert(0, (next, prev));
+                            prev = next;
+                            next = parent[prev];
+                        }
+                    }
+
+                    return result.ToImmutable();
+                }
+
+                foreach (var item in adjacent[current])
+                {
+                    if (!parent.ContainsKey(item))
+                    {
+                        parent[item] = current;
+                        queue.Enqueue(item);
+                    }
                 }
             }
 
-            var totalOrbits = CountTotalOrbits(orbits);
-            Console.WriteLine(totalOrbits);
+            return null;
         }
 
-#if true
+        private static ILookup<string, string> BuildEdgeMap(IEnumerable<(string, string)> edges)
+            => MakeBidirectionalEdges(edges).Distinct().ToLookup(x => x.from, x => x.to);
+
+        private static IEnumerable<(string from, string to)> MakeBidirectionalEdges(IEnumerable<(string a, string b)> edges)
+        {
+            foreach (var edge in edges)
+            {
+                yield return (edge.a, edge.b);
+                yield return (edge.b, edge.a);
+            }
+        }
+
+        private static IEnumerable<(string inner, string outer)> ParseOrbits(IEnumerable<string> orbits)
+            => orbits.Select(x => x.ToUpper().Split(')'))
+                     .Where(x => x.Length == 2)
+                     .Select(x => (inner: x[0], outer: x[1]));
+
         private static int CountTotalOrbits(ILookup<string, string> orbits, string node = "COM", int depth = 0)
             => depth + orbits[node].Sum(x => CountTotalOrbits(orbits, x, depth + 1));
-#else
-        private static int CountTotalOrbits(ILookup<string, string> orbits, string node = "COM", int depth = 0)
-        {
-            var result = depth;
-
-            foreach (var item in orbits[node])
-            {
-                result += CountTotalOrbits(orbits, item, depth + 1);
-            }
-
-            return result;
-        }
-#endif
 
         private static IEnumerable<string> TestInput =>
             new[]
@@ -61,7 +109,9 @@ namespace day06
                 "D)I",
                 "E)J",
                 "J)K",
-                "K)L"
+                "K)L",
+                "K)YOU",
+                "I)SAN"
             };
     }
 }
