11aa2d29c2fefeddaee6e77e164b3112b2488604
Steve Chovanec <steve@stevechovanec.com>
2020-03-15 01:04:58 -0400

    Day 15 part 2

--- 2019/day15/Program.cs
+++ 2019/day15/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 
@@ -12,17 +13,56 @@ namespace day15
             var code = File.ReadLines("input.txt").FirstOrDefault();
             var program = IntcodeMachine.ParseCode(code);
 
-            var walls = new HashSet<(long x, long y)>();
+            var movesToGenerator = CountMovesToGenerator(program, out var space, out var found);
+            Console.WriteLine($"Fewest Moves = {movesToGenerator}");
+
+            var longestTime = FindFarthestPoint(space, found);
+            Console.WriteLine($"Minutes to Fill = {longestTime}");
+        }
+
+        private static long FindFarthestPoint(HashSet<(long x, long y)> space, (long x, long y) start)
+        {
+            var distance = new Dictionary<(long x, long y), long>() { [start] = 0L };
+            var queue = new Queue<(long x, long y)>();
+            queue.Enqueue(start);
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                var nextDistance = distance[current] + 1;
+
+                foreach (var offset in AdjacentLocationOffsets)
+                {
+                    var adjacent = (current.x + offset.x, current.y + offset.y);
+                    if (space.Contains(adjacent))
+                    {
+                        if (!distance.TryGetValue(adjacent, out var adjacentDistance) || adjacentDistance > nextDistance)
+                        {
+                            distance[adjacent] = nextDistance;
+                            queue.Enqueue(adjacent);
+                        }
+                    }
+                }
+            }
+
+            return distance.Values.Max();
+        }
+
+        private static ImmutableArray<(long x, long y)> AdjacentLocationOffsets
+            = ImmutableArray.Create((1L, 0L), (-1L, 0L), (0L, 1L), (0L, -1L));
+
+        private static long CountMovesToGenerator(long[] program, out HashSet<(long x, long y)> space, out (long x, long y) found)
+        {
+            found = (0, 0);
 
             var position = (x: 0L, y: 0L);
             var visited = new HashSet<(long x, long y)>() { position };
 
-            var moves = new Stack<long>();
-
             var machine = new IntcodeMachine(program);
-            var found = false;
-
-            while (!found)
+            var walls = new HashSet<(long x, long y)>();
+            var moves = new Stack<long>();
+            long? movesToGenerator = null;
+            var done = false;
+            while (!done)
             {
                 var next = (from cmd in Enumerable.Range(1, 4)
                             let newPosition = CalculatePosition(position, cmd)
@@ -44,9 +84,14 @@ namespace day15
                         position = next.NewPosition;
                         moves.Push(next.Command);
                         if (result.Output == 2)
-                            found = true;
+                        {
+                            found = position;
+                            movesToGenerator = moves.Count;
+                        }
                         else if (result.Output != 1)
+                        {
                             throw new InvalidOperationException("Unexpected output");
+                        }
                     }
                 }
                 else if (moves.Count > 0)
@@ -59,9 +104,14 @@ namespace day15
 
                     position = CalculatePosition(position, cmd);
                 }
+                else
+                {
+                    done = true;
+                }
             }
 
-            Console.WriteLine($"Fewest Moves = {moves.Count}");
+            space = visited;
+            return movesToGenerator ?? 0;
         }
 
         private static (long x, long y) CalculatePosition((long x, long y) position, long command)
