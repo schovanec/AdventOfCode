5fd552db01caf0016472c80473f206cee41b3f1b
Steve Chovanec <steve@stevechovanec.com>
2020-04-26 18:03:31 -0400

    Day 23 completed

---
+++ 2019/day23/IntcodeMachine.cs
@@ -0,0 +1,270 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day23
+{
+    public sealed class IntcodeMachine
+    {
+        private readonly long[] memory;
+        private long programCounter = 0;
+        private long relativeBaseAddress = 0;
+
+        public const int DefaultMemorySize = 1 << 12;
+
+        private const long FlagStart = 100;
+
+        public IntcodeMachine(
+            ReadOnlySpan<long> program,
+            int memorySize = DefaultMemorySize)
+        {
+            this.memory = new long[Math.Max(program.Length, memorySize)];
+            program.CopyTo(memory);
+        }
+
+        public static long[] ParseCode(string code)
+            => (code ?? throw new ArgumentNullException(nameof(code))).Split(',').Select(long.Parse).ToArray();
+
+        public static bool RunProgram(long[] program, Action<long> write, Func<long?> input = default)
+        {
+            var machine = new IntcodeMachine(program);
+            while (true)
+            {
+                var result = machine.StepMany(input);
+                switch (result.State)
+                {
+                    case ExecuteState.HaveOutput:
+                        write?.Invoke(result.Output.Value);
+                        break;
+
+                    case ExecuteState.NeedInput:
+                        return false;
+
+                    case ExecuteState.Halted:
+                        return true;
+                }
+            }
+        }
+
+#if false
+        public static bool RunProgram(long[] program, Action<long> write, params string[] inputs)
+        {
+            var inputQueue = new Queue<long>();
+            foreach (var line in inputs ?? Enumerable.Empty<string>())
+            {
+                foreach (var ch in line)
+                    inputQueue.Enqueue(ch);
+
+                inputQueue.Enqueue(10); // newline
+            }
+
+            return RunProgram(program, write, inputQueue);
+        }
+#endif
+
+        public bool IsHalted => CurrentInstruction == Instruction.Halt;
+
+        public ExecuteResult StepMany(Func<long?> input)
+        {
+            while (true)
+            {
+                var result = Step(input ?? (() => default));
+                if (result.State != ExecuteState.Running)
+                    return result;
+            }
+        }
+
+#if false
+        public ExecuteResult StepMany(long input)
+        {
+            var inputQueue = new Queue<long>();
+            inputQueue.Enqueue(input);
+            return StepMany(() => inputQueue.TryDequeue(out var result) ? result : default);
+        }
+#endif
+
+        public ExecuteResult Step(Func<long?> input)
+        {
+            switch (CurrentInstruction)
+            {
+                case Instruction.Add:
+                    return DoBinaryInstruction((x, y) => x + y);
+
+                case Instruction.Multiply:
+                    return DoBinaryInstruction((x, y) => x * y);
+
+                case Instruction.ReadInput:
+                    return ReadInput(input);
+
+                case Instruction.WriteOutput:
+                    return WriteOutput();
+
+                case Instruction.JumpIfTrue:
+                    return DoConditionalJump(x => x != 0);
+
+                case Instruction.JumpIfFalse:
+                    return DoConditionalJump(x => x == 0);
+
+                case Instruction.CompareLess:
+                    return DoBinaryInstruction((x, y) => x < y ? 1 : 0);
+
+                case Instruction.CompareEqual:
+                    return DoBinaryInstruction((x, y) => x == y ? 1 : 0);
+
+                case Instruction.OffsetRelativeBase:
+                    return DoOffsetRelativeBase();
+
+                case Instruction.Halt:
+                    return ExecuteResult.Halted;
+
+                default:
+                    throw new InvalidOperationException("Unknown instruction");
+            }
+        }
+
+        private ExecuteResult DoBinaryInstruction(Func<long, long, long> func)
+        {
+            var a = GetParameter(1);
+            var b = GetParameter(2);
+
+            var result = func(a, b);
+
+            ref var target = ref GetParameter(3);
+            target = result;
+
+            programCounter += 4;
+
+            return ExecuteResult.Running;
+        }
+
+        private ExecuteResult DoConditionalJump(Predicate<long> condition)
+        {
+            var a = GetParameter(1);
+
+            if (condition(a))
+                programCounter = GetParameter(2);
+            else
+                programCounter += 3;
+
+            return ExecuteResult.Running;
+        }
+
+        private ExecuteResult DoOffsetRelativeBase()
+        {
+            relativeBaseAddress += GetParameter(1);
+            programCounter += 2;
+            return ExecuteResult.Running;
+        }
+
+        private ExecuteResult ReadInput(Func<long?> input)
+        {
+            var inputValue = input?.Invoke();
+            if (!inputValue.HasValue)
+                return ExecuteResult.NeedInput;
+
+            ref var target = ref GetParameter(1);
+            target = inputValue.Value;
+
+            programCounter += 2;
+
+            return ExecuteResult.Running;
+        }
+
+        private ExecuteResult WriteOutput()
+        {
+            var outputValue = GetParameter(1);
+            programCounter += 2;
+
+            return ExecuteResult.HaveOutput(outputValue);
+        }
+
+        private Instruction CurrentInstruction
+            => (Instruction)(memory[programCounter] % FlagStart);
+
+        private ref long GetParameter(long index)
+        {
+            ref var parameter = ref memory[programCounter + index];
+            switch (GetParameterMode(index))
+            {
+                case Mode.Immediate:
+                    return ref parameter;
+
+                case Mode.Indirect:
+                    return ref memory[parameter];
+
+                case Mode.Relative:
+                    return ref memory[relativeBaseAddress + parameter];
+
+                default:
+                    throw new InvalidOperationException("Invalid parameter mode");
+            }
+        }
+
+        private Mode GetParameterMode(long index)
+            => GetParameterFlags(index) switch
+            {
+                0 => Mode.Indirect,
+                1 => Mode.Immediate,
+                2 => Mode.Relative,
+                _ => throw new InvalidOperationException("Invalid paramter flags")
+            };
+
+        private long GetParameterFlags(long index)
+            => index switch
+            {
+                1 => (memory[programCounter] / (FlagStart * 1)) % 10,
+                2 => (memory[programCounter] / (FlagStart * 10)) % 10,
+                3 => (memory[programCounter] / (FlagStart * 100)) % 10,
+                _ => throw new ArgumentOutOfRangeException(nameof(index), index, "Invalid parameter index")
+            };
+
+        private enum Instruction
+        {
+            Add = 1,
+            Multiply = 2,
+            ReadInput = 3,
+            WriteOutput = 4,
+            JumpIfTrue = 5,
+            JumpIfFalse = 6,
+            CompareLess = 7,
+            CompareEqual = 8,
+            OffsetRelativeBase = 9,
+            Halt = 99
+        }
+
+        private enum Mode
+        {
+            Immediate,
+            Indirect,
+            Relative
+        }
+    }
+
+    public enum ExecuteState
+    {
+        Running,
+        Halted,
+        NeedInput,
+        HaveOutput
+    }
+
+    public struct ExecuteResult
+    {
+        public static readonly ExecuteResult Running = new ExecuteResult(ExecuteState.Running);
+        public static readonly ExecuteResult Halted = new ExecuteResult(ExecuteState.Halted);
+        public static readonly ExecuteResult NeedInput = new ExecuteResult(ExecuteState.NeedInput);
+
+        public ExecuteResult(ExecuteState state, long? output = default)
+        {
+            State = state;
+            Output = output;
+        }
+
+        public ExecuteState State { get; }
+
+        public long? Output { get; }
+
+        public static ExecuteResult HaveOutput(long value)
+            => new ExecuteResult(ExecuteState.HaveOutput, value);
+    }
+}
