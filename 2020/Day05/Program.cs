6285ee74fee7ae9fa0373a17fd6863847a25933b
Steve Chovanec <schovanec@gmail.com>
2020-12-05 13:48:18 -0500

    2020 - Day 5

---
+++ 2020/Day05/Program.cs
@@ -0,0 +1,28 @@
+﻿    using System;
+    using System.IO;
+    using System.Linq;
+
+    namespace Day05
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var file = args.DefaultIfEmpty("input.txt").First();
+
+                var seats = File.ReadLines(file)
+                                .Select(x => x.Aggregate(0, (v, c) => (v << 1) + c switch { 'B' or 'R' => 1, _ => 0 }))
+                                .OrderByDescending(id => id)
+                                .ToList();
+
+                var max = seats.First();
+                Console.WriteLine($"Part 1 Result: {max}");
+
+                var missing = seats.Zip(seats.Skip(1), (a, b) => (a, b))
+                                   .Where(x => x.a - x.b > 1)
+                                   .Select(x => x.b + 1)
+                                   .First();
+                Console.WriteLine($"Part 2 Result: {missing}");
+            }
+        }
+    }
