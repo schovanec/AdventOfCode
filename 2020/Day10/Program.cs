fa88a220b488851ebe1fd03d53f2b68554cadf99
Steve Chovanec <schovanec@gmail.com>
2020-12-10 20:28:51 -0500

    Refactoring

--- 2020/Day10/Program.cs
+++ 2020/Day10/Program.cs
@@ -8,61 +8,60 @@ namespace Day10
 {
     class Program
     {
+        const int MaxDiff = 3;
+
         static void Main(string[] args)
         {
             var file = args.DefaultIfEmpty("input.txt").First();
             var adapters = File.ReadLines(file)
-                               .Select(long.Parse)
-                               .OrderBy(x => x)
-                               .ToArray();
+                               .Select(int.Parse)
+                               .OrderBy(x => x);
 
-            Part1(adapters);
-            Part2(adapters);
+            var builder = ImmutableArray.CreateBuilder<int>();
+            builder.Add(0);
+            builder.AddRange(adapters);
+            builder.Add(builder.Last() + MaxDiff);
+
+            var chain = builder.ToImmutable();
+
+            Part1(chain);
+            Part2(chain);
         }
 
-        static void Part1(long[] adapters)
+        static void Part1(ImmutableArray<int> chain)
         {
-            var builtInAdapter = adapters.Last() + 3;
-
-            var chain = ImmutableList.Create(0L)
-                                     .Concat(adapters)
-                                     .Append(builtInAdapter);
-
             var differences = chain.Zip(chain.Skip(1), (a, b) => b - a)
                                    .GroupBy(x => x, (x, v) => (key: x, count: v.Count()))
                                    .ToArray();
 
             var product = differences.Select(x => x.count).Aggregate((a, b) => a * b);
-            Console.WriteLine($"Differences: {string.Join(", ", differences.Select(x => $"{x.key}={x.count}"))}");
             Console.WriteLine($"Part 1 Result = {product}");
         }
 
-        static void Part2(long[] adapters)
+        static void Part2(ImmutableArray<int> chain)
         {
-            var builtInAdapter = adapters.Last() + 3;
-            var count = CountAdapterArrangements(0, adapters, builtInAdapter);
+            var count = CountAdapterArrangements(chain.AsSpan());
             Console.WriteLine($"Part 2 Result = {count}");
         } 
 
-        static long CountAdapterArrangements(long initial, ReadOnlySpan<long> adapters, long final)
-            => CountAdapterArrangements(initial, adapters, final, new Dictionary<(long, int), long>());
+        static long CountAdapterArrangements(ReadOnlySpan<int> adapters)
+            => CountAdapterArrangements(adapters, new Dictionary<(int, int), long>());
 
-        static long CountAdapterArrangements(long initial, ReadOnlySpan<long> adapters, long final, Dictionary<(long, int), long> cache)
+        static long CountAdapterArrangements(ReadOnlySpan<int> adapters, Dictionary<(int, int), long> cache)
         {
-            var key = (initial, adapters.Length);
-            if (cache.TryGetValue(key, out var result))
-                return result;
+            if (adapters.Length <= 2)
+                return 1;
 
-            result = 0L;
+            var key = (adapters[0], adapters.Length);
+            if (!cache.TryGetValue(key, out var result))
+            {
+                result = 0L;
 
-            for (var i = 0; i < adapters.Length && adapters[i] <= initial + 3; ++i)
-                result += CountAdapterArrangements(adapters[i], adapters.Slice(i + 1), final, cache);
+                for (var i = 1; i < adapters.Length && adapters[i] <= adapters[0] + MaxDiff; ++i)
+                    result += CountAdapterArrangements(adapters.Slice(i), cache);
 
-            if (final <= initial + 3)
-                ++result;
-
-            cache.Add(key, result);
-
+                cache.Add(key, result);
+            }
             return result;
         }
     }
