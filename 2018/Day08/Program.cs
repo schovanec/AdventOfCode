970495a56a5d70f107517960c9fd28119b2a572c
Steve Chovanec <schovanec@gmail.com>
2021-04-17 13:57:52 -0400

    Refectoring

--- 2018/Day08/Program.cs
+++ 2018/Day08/Program.cs
@@ -11,42 +11,35 @@ namespace Day08
         static void Main(string[] args)
         {
             var fileName = args.DefaultIfEmpty("input.txt").First();
-            var input = Parse(File.ReadAllText(fileName));
-
-            var tree = BuildTree(input);
+            var tree = BuildTree(ParseInput(File.ReadAllText(fileName)));
 
             Console.WriteLine($"Part 1 Result = {tree.SumMetadata()}");
             Console.WriteLine($"Part 2 Result = {tree.GetValue()}");
         }
 
-        private static Node BuildTree(IReadOnlyList<int> input)
+        private static Node BuildTree(IEnumerable<int> input)
+            => BuildTree(new Queue<int>(input));
+
+        private static Node BuildTree(Queue<int> input)
         {
-            var pos = 0;
-            return BuildNode();
+            var childCount = input.Dequeue();
+            var metaCount = input.Dequeue();
 
-            Node BuildNode()
-            {
-                var childCount = input[pos++];
-                var metaCount = input[pos++];
+            var children = ImmutableArray.CreateBuilder<Node>(childCount);
+            for (var i = 0; i < childCount; ++i)
+                children.Add(BuildTree(input));
 
-                var children = Enumerable.Range(0, childCount)
-                                         .Select(_ => BuildNode())
-                                         .ToImmutableArray();
+            var metadata = ImmutableArray.CreateBuilder<int>(metaCount);
+            for (var i = 0; i < metaCount; ++i)
+                metadata.Add(input.Dequeue());
 
-                var metadata = Enumerable.Range(pos, metaCount)
-                                         .Select(i => input[i])
-                                         .ToImmutableArray();
-
-                pos += metaCount;
-
-                return new Node(children, metadata);
-            }
+            return new Node(
+                children.ToImmutable(),
+                metadata.ToImmutable());
         }
 
-        private static ImmutableList<int> Parse(ReadOnlySpan<char> input)
+        private static IEnumerable<int> ParseInput(string input)
         {
-            var result = ImmutableList.CreateBuilder<int>();
-
             var start = -1;
             var current = 0;
             while (current < input.Length)
@@ -58,7 +51,7 @@ namespace Day08
                 }
                 else if (start >= 0 && current > start)
                 {
-                    result.Add(int.Parse(input.Slice(start, current - start)));
+                    yield return int.Parse(input.AsSpan().Slice(start, current - start));
                     start = -1;
                 }
 
@@ -66,9 +59,7 @@ namespace Day08
             }
 
             if (start >= 0 && current > start)
-                result.Add(int.Parse(input.Slice(start, current - start)));
-
-            return result.ToImmutable();
+                yield return int.Parse(input.AsSpan().Slice(start, current - start));
         }
 
         record Node(ImmutableArray<Node> Children, ImmutableArray<int> Metadata)
