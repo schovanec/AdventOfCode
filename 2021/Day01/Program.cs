8f492cf6ab345b4a375c1e9828abb6dd994742af
Steve Chovanec <steve@stevechovanec.com>
2021-12-01 19:18:15 -0500

    2021 - Day 1

---
+++ 2021/Day01/Program.cs
@@ -0,0 +1,17 @@
+﻿var input = File.ReadLines(args.FirstOrDefault() ?? "input.txt")
+                .Select(int.Parse)
+                .ToList();
+
+var count1 = input.Zip(input.Skip(1))
+                  .Count(x => x.First < x.Second);
+
+Console.WriteLine($"Part 1 Result = {count1}");
+
+var sums = input.Zip(input.Skip(1), input.Skip(2))
+                .Select(x => x.First + x.Second + x.Third)
+                .ToList();
+
+var count2 = sums.Zip(sums.Skip(1))
+                 .Count(x => x.First < x.Second);
+
+Console.WriteLine($"Part 2 Result = {count2}");
