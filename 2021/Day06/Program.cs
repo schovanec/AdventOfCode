a246caa3c19f43b1ea233e8bd04c0e72ad7d4610
Steve Chovanec <steve@stevechovanec.com>
2021-12-06 18:13:53 -0500

    2021 - Day 6

---
+++ 2021/Day06/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = File.ReadLines(args.FirstOrDefault() ?? "input.txt")
+                .SelectMany(x => x.Split(','))
+                .Select(int.Parse)
+                .ToList();
+
+var fish = new long[9];
+foreach (var age in input)
+  fish[age]++;
+
+for (var i = 0; i < 256; ++i)
+{
+  var zeroIndex = (i % 9);
+  var spawnAtIndex = ((i + 7) % 9);
+
+  fish[spawnAtIndex] += fish[zeroIndex];
+
+  if (i + 1 == 80)
+    Console.WriteLine($"Part 1 Result = {fish.Sum()}");
+}
+
+Console.WriteLine($"Part 2 Result = {fish.Sum()}");
