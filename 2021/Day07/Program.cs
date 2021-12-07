c65f2fa0b19963451cc5ed9ec83b37275667aa89
Steve Chovanec <steve@stevechovanec.com>
2021-12-07 18:31:31 -0500

    2020 - Day 7

---
+++ 2021/Day07/Program.cs
@@ -0,0 +1,18 @@
+﻿var input = File.ReadLines(args.FirstOrDefault() ?? "input.txt")
+                .SelectMany(x => x.Split(','))
+                .Select(int.Parse)
+                .ToList();
+
+var min = input.Min();
+var max = input.Max();
+var range = Enumerable.Range(min, max - min + 1);
+
+var best1 = range.Min(i => input.Sum(n => Distance(n, i)));
+Console.WriteLine($"Part 1 Result = {best1}");
+
+var best2 = range.Min(i => input.Sum(n => SumTo(Distance(n, i))));
+Console.WriteLine($"Part 2 Result = {best2}");
+
+int Distance(int a, int b) => Math.Abs(a - b);
+
+int SumTo(int n) => (n * (n + 1)) / 2;
