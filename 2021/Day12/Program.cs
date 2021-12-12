e6d72d6c8316ecd6864f37386a3e6b8120d04671
Steve Chovanec <steve@stevechovanec.com>
2021-12-12 01:16:22 -0500

    Refactoring

--- 2021/Day12/Program.cs
+++ 2021/Day12/Program.cs
@@ -1,42 +1,34 @@
 ﻿using System.Collections.Immutable;
-using System.Text;
 
 var edges = File.ReadLines(args.FirstOrDefault() ?? "input.txt")
                 .Select(x => x.Split('-'))
                 .SelectMany(x => new[] { (from: x[0], to: x[1]), (from: x[1], to: x[0]) })
                 .ToLookup(x => x.from, x => x.to);
 
-var count1 = FindAllPathsPart1(edges, "start", "end").Count();
-Console.WriteLine($"Part 1 Result = {count1}");
+var paths1 = FindAllPaths(
+  edges, "start", "end",
+  (n, v) => IsLargeCave(n) || v.GetValueOrDefault(n) == 0);
 
-var count2 = FindAllPathsPart2(edges, "start", "end").Count();
-Console.WriteLine($"Part 2 Result = {count2}");
+Console.WriteLine($"Part 1 Result = {paths1.Count()}");
 
-IEnumerable<Path> FindAllPathsPart1(ILookup<string, string> edges, string from, string to)
-  => FindAllPathsInternal(edges,
-                          from,
-                          to,
-                          (n, v) => IsLargeCave(n) || !v.ContainsKey(n));
+var paths2 = FindAllPaths(
+  edges, "start", "end",
+  (n, v) => n switch
+  {
+    "start" or "end" => v.GetValueOrDefault(n) == 0,
+    _ when IsSmallCave(n) => v.GetValueOrDefault(n) == 0
+                             || !v.Any(x => IsSmallCave(x.Key) && x.Value > 1),
+    _ => true
+  });
 
-IEnumerable<Path> FindAllPathsPart2(ILookup<string, string> edges, string from, string to)
-  => FindAllPathsInternal(edges,
-                          from,
-                          to,
-                          (n, v) => n switch
-                          {
-                            "start" or "end" => !v.ContainsKey(n),
-                            _ when IsSmallCave(n) => !v.ContainsKey(n) || !v.Any(x => IsSmallCave(x.Key) && x.Value > 1),
-                            _ => true
-                          });
+Console.WriteLine($"Part 2 Result = {paths2.Count()}");
 
-IEnumerable<Path> FindAllPathsInternal(ILookup<string, string> edges,
-                                       string from,
-                                       string to,
-                                       Func<string, ImmutableDictionary<string, int>, bool> canVisitNode,
-                                       ImmutableDictionary<string, int>? visited = default)
+IEnumerable<Path> FindAllPaths(ILookup<string, string> edges, string from, string to,
+                               Func<string, Dictionary<string, int>, bool> canVisitNode,
+                               Dictionary<string, int>? visited = default)
 {
-  visited ??= ImmutableDictionary.Create<string, int>();
-  visited = visited.SetItem(from, visited.GetValueOrDefault(from) + 1);
+  visited ??= new Dictionary<string, int>();
+  visited[from] = visited.GetValueOrDefault(from) + 1;
 
   var path = new Path(from);
 
@@ -50,33 +42,17 @@ IEnumerable<Path> FindAllPathsInternal(ILookup<string, string> edges,
     {
       if (canVisitNode(node, visited))
       {
-        foreach (var result in FindAllPathsInternal(edges, node, to, canVisitNode, visited))
+        foreach (var result in FindAllPaths(edges, node, to, canVisitNode, visited))
           yield return path with { Tail = result };
       }
     }
   }
+
+  visited[from]--;
 }
 
 bool IsLargeCave(string node) => node.All(char.IsUpper);
 
 bool IsSmallCave(string node) => node is not "start" or "end" && !IsLargeCave(node);
 
-record Path(string Node, Path? Tail = null)
-{
-  public override string ToString()
-  {
-    var result = new StringBuilder();
-
-    var current = this;
-    while (current != null)
-    {
-      if (result.Length > 0)
-        result.Append(',');
-
-      result.Append(current.Node);
-      current = current.Tail;
-    }
-
-    return result.ToString();
-  }
-}
+record Path(string Node, Path? Tail = null);
