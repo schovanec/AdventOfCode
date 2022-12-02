5526bef480f2014bdcab397d9544f28528706bba
Steve Chovanec <git@stevechovanec.com>
2022-12-01 19:54:19 -0500

    2022 - Day 1

---
+++ 2022/Day01/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = File.ReadLines(args.FirstOrDefault() ?? "input.txt");
+
+var top3 = EnumTotals(input).OrderByDescending(x => x).Take(3).ToArray();
+
+Console.WriteLine($"Part1 Result = {top3.First()}");
+Console.WriteLine($"Part2 Result = {top3.Sum()}");
+
+IEnumerable<int> EnumTotals(IEnumerable<string> input)
+{
+  var sum = 0;
+  foreach (var item in input)
+  {
+    if (item.Length == 0)
+    {
+      yield return sum;
+      sum = 0;
+    }
+    else
+    {
+      sum += int.Parse(item);
+    }
+  }
+}
