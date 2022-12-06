f408b371f275da4dc42016d76dcce7617440f8e9
Steve Chovanec <git@stevechovanec.com>
2022-12-06 18:26:43 -0500

    2022 - Day 6

---
+++ 2022/Day06/Program.cs
@@ -0,0 +1,13 @@
+﻿var input = File.ReadLines(args.FirstOrDefault() ?? "input.txt").First();
+
+var packetMarkerPosition = FindMarkers(input, 4).First();
+Console.WriteLine($"Part 1 Result = {packetMarkerPosition}");
+
+var messageMarkerPosition = FindMarkers(input, 14).First();
+Console.WriteLine($"Part 1 Result = {messageMarkerPosition}");
+
+static IEnumerable<int> FindMarkers(string input, int size)
+  => from i in Enumerable.Range(size, input.Length - size)
+     let prev = Enumerable.Range(i - size, size).Select(n => input[n])
+     where prev.Distinct().Count() == size
+     select i;
