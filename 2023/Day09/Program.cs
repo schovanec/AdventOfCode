18a6373b6bec18e79f6ed16a3dbaab7d9edeb632
schovanec <git@stevechovanec.com>
2023-12-09 22:32:10 +0000

    2023 - Day 9

---
+++ 2023/Day09/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+
+var input = File.ReadLines(args.FirstOrDefault() ?? "input.txt")
+                .Select(x => x.Split(' ').Select(long.Parse))
+                .Select(x => x.ToImmutableArray());
+
+var result1 = input.Sum(FindNextValue);
+Console.WriteLine($"Part 1 Result = {result1}");
+
+var result2 = input.Sum(FindPrevValue);
+Console.WriteLine($"Part 2 Result = {result2}");
+
+long FindNextValue(ImmutableArray<long> seq)
+  => seq.All(x => x == 0)
+    ? 0L
+    : seq.Last() + FindNextValue(FindDifferences(seq));
+
+long FindPrevValue(ImmutableArray<long> seq)
+  => seq.All(x => x == 0)
+    ? 0L
+    : seq.First() - FindPrevValue(FindDifferences(seq));
+
+ImmutableArray<long> FindDifferences(ImmutableArray<long> seq)
+  => seq.Zip(seq.Skip(1), (a, b) => b - a)
+        .ToImmutableArray();
