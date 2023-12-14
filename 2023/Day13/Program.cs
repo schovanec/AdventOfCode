1c3e033ffa6a1303bd0903236bfaf74e5c6c0712
schovanec <git@stevechovanec.com>
2023-12-14 00:00:42 +0000

    2023 - Day 13 - Part 2

--- 2023/Day13/Program.cs
+++ 2023/Day13/Program.cs
@@ -1,24 +1,37 @@
 ﻿var input = ParseInput(File.ReadLines(args.FirstOrDefault() ?? "input.txt")).ToArray();
 
-var result1 = input.Sum(x => (FindHorizontalReflection(x) * 100) ?? FindVerticalReflection(x));
+var result1 = input.Sum(x => (FindHorizontalReflection(x, false) * 100) ?? FindVerticalReflection(x, false));
 Console.WriteLine($"Part 1 Result = {result1}");
 
-static int? FindHorizontalReflection(List<string> pattern)
+var result2 = input.Sum(x => (FindHorizontalReflection(x, true) * 100) ?? FindVerticalReflection(x, true));
+Console.WriteLine($"Part 2 Result = {result2}");
+
+static int? FindHorizontalReflection(List<string> pattern, bool allowSmudge)
 {
   return (from pos in Enumerable.Range(1, pattern.Count - 1)
-          where IsReflection(pos, pattern)
+          where IsReflection(pos, pattern, allowSmudge)
           select (int?)pos).FirstOrDefault();
 }
 
-static int? FindVerticalReflection(List<string> pattern)
-  => FindHorizontalReflection(Transpose(pattern));
+static int? FindVerticalReflection(List<string> pattern, bool allowSmudge)
+  => FindHorizontalReflection(Transpose(pattern), allowSmudge);
 
-static bool IsReflection(int pos, List<string> pattern)
-  => !(from i in Enumerable.Range(0, Math.Min(pos, pattern.Count - pos))
-       let top = pos - i - 1
-       let bot = pos + i
-       where pattern[top] != pattern[bot]
-       select i).Any();
+static bool IsReflection(int pos, List<string> pattern, bool allowSmudge)
+{
+  var badPairs = from i in Enumerable.Range(0, Math.Min(pos, pattern.Count - pos))
+                 let top = pattern[pos - i - 1]
+                 let bot = pattern[pos + i]
+                 where top != bot
+                 select (top, bot);
+
+  if (!allowSmudge)
+    return !badPairs.Any();
+
+  if (badPairs.ToArray() is [(var badTop, var badBot)])
+    return badTop.Zip(badBot).Count(x => x.First != x.Second) == 1;
+  else
+    return false;
+}
 
 static List<string> Transpose(List<string> pattern)
 {
