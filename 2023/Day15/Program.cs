276c56c481d12de7f1409a230288bd93ed050151
schovanec <git@stevechovanec.com>
2023-12-15 18:05:34 +0000

    2023 - Day 15 - Part 1

---
+++ 2023/Day15/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = File.ReadAllLines(args.FirstOrDefault() ?? "input.txt")
+                .SelectMany(line => line.Split(','))
+                .ToArray();
+
+var result1 = input.Sum(HASH);
+Console.WriteLine($"Part 1 Result = {result1}");
+
+static int HASH(string input)
+{
+  return input.Aggregate(0, Step);
+
+  int Step(int current, char ch)
+    => ((current + (int)ch) * 17) % 256;
+}
