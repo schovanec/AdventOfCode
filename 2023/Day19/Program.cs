b64bb41b7d0a62d2132ad986af10d437651c2aee
schovanec <git@stevechovanec.com>
2023-12-22 06:23:04 +0000

    2023 - Day 19 - Part 2

--- 2023/Day19/Program.cs
+++ 2023/Day19/Program.cs
@@ -1,12 +1,15 @@
-﻿// using Bounds = (int min, int max);
+﻿using System.Collections;
 
 var (rules, parts) = ParseInput(File.ReadAllLines(args.FirstOrDefault() ?? "input.txt"));
 
 var acceptedParts = parts.Where(p => EvaluatePart(p, rules) == "A");
-var result1 = acceptedParts.Sum(p => p.TotalRating);
+var result1 = acceptedParts.Sum(GetPartTotalRating);
 Console.WriteLine($"Part 1 Result = {result1}");
 
-static string EvaluatePart(Part part, Dictionary<string, RuleSet> rules, string startRule = "in")
+var result2 = CountAcceptedCombinations(rules);
+Console.WriteLine($"Part 2 Result = {result2}");
+
+static string EvaluatePart(Part<int> part, Dictionary<string, RuleSet> rules, string startRule = "in")
 {
   var current = rules[startRule];
   while (current != null)
@@ -21,30 +24,81 @@ static string EvaluatePart(Part part, Dictionary<string, RuleSet> rules, string 
   return "";
 }
 
-// static long CountAcceptedCombinations(Dictionary<string, RuleSet> rules, string startRule = "in")
-// {
+static int GetPartTotalRating(Part<int> part)
+  => part.X + part.M + part.A + part.S;
 
-// }
+static long CountAcceptedCombinations(Dictionary<string, RuleSet> allRuleSets, string startRule = "in", int min = 1, int max = 4000)
+{
+  var rb = new Bounds(min, max);
+  var bounds = new Part<Bounds>(rb, rb, rb, rb);
+  return SearchRulesByName(startRule, bounds);
 
-static (Dictionary<string, RuleSet> rules, Part[] parts) ParseInput(string[] input)
+  long SearchRulesByName(string name, Part<Bounds> bounds)
+    => name switch
+    {
+      "A" => GetProductOfSizes(bounds),
+      "R" => 0L,
+      _ => SearchRules(allRuleSets[name].Rules, bounds)
+    };
+
+  long SearchRules(ReadOnlySpan<Rule> rules, Part<Bounds> bounds)
+  {
+    if (rules.IsEmpty)
+      return 0L;
+
+    switch (rules[0])
+    {
+      case AlwaysRule(var target):
+        return SearchRulesByName(target, bounds);
+
+      case ConditionRule(var key, _, _, var target) rule:
+        var trueTotal = SearchRulesByName(
+          target,
+          bounds.With(key, rule.LimitBoundsForTrue(bounds[key])));
+
+        var falseTotal = SearchRules(
+          rules[1..],
+          bounds.With(key, rule.LimitBoundsForFalse(bounds[key])));
+
+        return trueTotal + falseTotal;
+
+      default:
+        return 0L;
+    }
+  }
+}
+
+static long GetProductOfSizes(Part<Bounds> bounds) => bounds.Aggregate(1L, (a, b) => a * b.Size);
+
+static (Dictionary<string, RuleSet> rules, Part<int>[] parts) ParseInput(string[] input)
 {
   var splitPos = Array.FindIndex(input, string.IsNullOrWhiteSpace);
 
   var rules = input[..splitPos].Select(RuleSet.Parse)
                                .ToDictionary(rs => rs.Name);
 
-  var parts = input[(splitPos + 1)..].Select(Part.Parse).ToArray();
+  var parts = input[(splitPos + 1)..].Select(ParsePart).ToArray();
 
   return (rules, parts);
 }
 
+static Part<int> ParsePart(string part)
+{
+  var ratings = part.Trim('{', '}')
+                    .Split(',')
+                    .Select(p => p.Split('=', 2))
+                    .Select(p => (name: p[0], value: int.Parse(p[1])));
+
+  return ratings.Aggregate(
+    default(Part<int>),
+    (a, p) => a.With(p.name[0], p.value));
+}
+
+
 record RuleSet(string Name, Rule[] Rules)
 {
-  public string Evaluate(Part part)
-    => Rules.First(r => r.IsValidFor(part)).Target;
-
-  public override string ToString()
-    => $"{Name}: [{string.Join(", ", Rules.AsEnumerable())}]";
+  public string Evaluate(Part<int> part)
+    => Rules.First(r => r.IsTrueFor(part)).Target;
 
   public static RuleSet Parse(string input)
   {
@@ -60,7 +114,7 @@ record RuleSet(string Name, Rule[] Rules)
 
 abstract record Rule(string Target)
 {
-  public abstract bool IsValidFor(Part part);
+  public abstract bool IsTrueFor(Part<int> part);
 
   public static Rule Parse(string input)
   {
@@ -76,17 +130,33 @@ abstract record Rule(string Target)
 
 record AlwaysRule(string Target) : Rule(Target)
 {
-  public override bool IsValidFor(Part part) => true;
+  public override bool IsTrueFor(Part<int> part) => true;
 }
 
 record ConditionRule(char Key, char Op, int Value, string Target) : Rule(Target)
 {
-  public override bool IsValidFor(Part part)
+  public override bool IsTrueFor(Part<int> part)
     => Op switch
     {
       '<' => part[Key] < Value,
       '>' => part[Key] > Value,
       _ => false
+    };
+
+  public Bounds LimitBoundsForTrue(Bounds bounds)
+    => Op switch
+    {
+      '<' => bounds.LimitMax(Value - 1),
+      '>' => bounds.LimitMin(Value + 1),
+      _ => bounds
+    };
+
+  public Bounds LimitBoundsForFalse(Bounds bounds)
+    => Op switch
+    {
+      '<' => bounds.LimitMin(Value),
+      '>' => bounds.LimitMax(Value),
+      _ => bounds
     };
 
   public static ConditionRule Parse(string condition, string target)
@@ -98,9 +168,10 @@ record ConditionRule(char Key, char Op, int Value, string Target) : Rule(Target)
   }
 }
 
-record struct Part(int X, int M, int A, int S)
+record struct Part<T>(T X, T M, T A, T S) : IEnumerable<T>
+  where T : struct
 {
-  public readonly int this[char key] => key switch
+  public readonly T this[char key] => key switch
   {
     'x' => X,
     'm' => M,
@@ -109,24 +180,34 @@ record struct Part(int X, int M, int A, int S)
     _ => default
   };
 
-  public readonly int TotalRating => X + M + A + S;
-
-  public static Part Parse(string part)
+  public Part<T> With(char key, T value)
+  => key switch
   {
-    var ratings = part.Trim('{', '}')
-                      .Split(',')
-                      .Select(p => p.Split('=', 2))
-                      .Select(p => (name: p[0], value: int.Parse(p[1])));
+    'x' => this with { X = value },
+    'm' => this with { M = value },
+    'a' => this with { A = value },
+    's' => this with { S = value },
+    _ => this
+  };
 
-    return ratings.Aggregate(
-      default(Part),
-      (a, p) => p.name switch
-      {
-        "x" => a with { X = p.value },
-        "m" => a with { M = p.value },
-        "a" => a with { A = p.value },
-        "s" => a with { S = p.value },
-        _ => a
-      });
+  public readonly IEnumerator<T> GetEnumerator()
+  {
+    yield return X;
+    yield return M;
+    yield return A;
+    yield return S;
   }
+
+  IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+record struct Bounds(int Min, int Max)
+{
+  public readonly int Size => Max - Min + 1;
+
+  public Bounds LimitMin(int limit)
+    => this with { Min = Math.Max(limit, Min) };
+
+  public Bounds LimitMax(int limit)
+    => this with { Max = Math.Min(limit, Max) };
 }
