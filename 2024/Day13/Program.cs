313c6975df3ffb0c31e261a974aee17bfecdb11b
schovanec <git@stevechovanec.com>
2024-12-14 18:25:23 +0000

    2024 - Day 13 - Part 2

--- 2024/Day13/Program.cs
+++ 2024/Day13/Program.cs
@@ -1,17 +1,32 @@
-﻿var games = Game.ParseMany(File.ReadAllLines(args.FirstOrDefault() ?? "input.txt"))
+﻿var games = Game.ParseMany(File.ReadAllLines(args.FirstOrDefault() ?? "input.txt")) //@"..\..\..\sample1.txt"))
                 .ToList();
 
-var result1 = games.Sum(g => FindBestCost(g, CalculateCost));
+var result1 = games.Sum(g => FindBest(g, CalculateCost));
 Console.WriteLine($"Part 1 Result = {result1}");
 
-static int FindBestCost(Game game, Func<int, int, int> costFunction)
-  => game.EnumValidMoves()
-         .Select(x => costFunction(x.a, x.b))
-         .DefaultIfEmpty(0)
-         .Min();
+var harder = games.Select(g => g.MovePrize(10000000000000, 10000000000000)).ToList();
+var result2 = harder.Sum(g => FindBest(g, CalculateCost));
+Console.WriteLine($"Part 2 Result = {result2}");
 
-static int CalculateCost(int aPresses, int bPresses)
+static long CalculateCost(long aPresses, long bPresses)
   => (aPresses * 3) + (bPresses * 1);
+
+static long FindBest(Game game, Func<long, long, long> cost)
+{
+  var detT = Determinant(game.ButtonA, game.ButtonB);
+  var detA = Determinant(game.Prize, game.ButtonB);
+  var detB = Determinant(game.ButtonA, game.Prize);
+
+  var aPresses = detA / detT;
+  var bPresses = detB / detT;
+
+  if (game.GetClawPosition(aPresses, bPresses) == game.Prize)
+    return cost(aPresses, bPresses);
+  else
+    return 0;
+}
+
+static long Determinant(Vector a, Vector b) => (a.X * b.Y) - (b.X * a.Y);
 
 record struct Vector(long X, long Y)
 {
@@ -26,17 +41,13 @@ record struct Vector(long X, long Y)
 
 record Game(Vector ButtonA, Vector ButtonB, Vector Prize)
 {
-  public IEnumerable<(int a, int b)> EnumValidMoves()
-    => from i in Enumerable.Range(0, MaxButtonA)
-       from j in Enumerable.Range(0, MaxButtonB)
-       let endX = (ButtonA.X * i) + (ButtonB.X * j)
-       let endY = (ButtonA.Y * i) + (ButtonB.Y * j)
-       where Prize == new Vector(endX, endY)
-       select (i, j);
+  public Vector GetClawPosition(long aPresses, long bPresses)
+    => new Vector(
+      (ButtonA.X * aPresses) + (ButtonB.X * bPresses),
+      (ButtonA.Y * aPresses) + (ButtonB.Y * bPresses));
 
-  public int MaxButtonA => (int)Math.Min(Prize.X / ButtonA.X + 1, Prize.Y / ButtonA.Y + 1);
-
-  public int MaxButtonB => (int)Math.Min(Prize.X / ButtonB.X + 1, Prize.Y / ButtonB.Y + 1);
+  public Game MovePrize(long offsetX, long offsetY)
+    => this with { Prize = new Vector(Prize.X + offsetX, Prize.Y + offsetY) };
 
   public static IEnumerable<Game> ParseMany(IReadOnlyList<string> input)
   {
